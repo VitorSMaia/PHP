39b8d24bb80415488ef1c4ff3cd1e728417fce7c
Joao vitor maia <joao.vitor@wirelink.com.br>
2021-08-19 20:00:22 -0300

    Primeiro Commit

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
-<?php 
-echo "Vitor Maia"
-?>
+<?php
+$categorias = [];
+
+$categorias[0] = 'Infantil';
+$categorias[1] = 'Adolecente';
+$categorias[2] = 'Adulto';
+$categorias[3] = 'Idoso';
+
+$nome = "Vitor";
+$idade = 20;
+
+if($idade >= 6 AND $idade <= 12)
+{
+    for($i = 0;$i < count($categorias);$i++)
+    {
+        if($categorias[$i] == 'Infantil')
+        {   
+            echo "O Competidor ". $nome." Com Idade,".$idade.". Está na categoria: ".$categorias[$i];
+        };
+    };
+}else if($idade >= 13 AND $idade <= 18)
+{
+    for($i = 0;$i < count($categorias);$i++)
+    {
+        if($categorias[$i] == 'Adolecente')
+        {   
+            echo "O Competidor ". $nome." Com Idade,".$idade.". Está na categoria: ".$categorias[$i];
+        };
+    };
+}else{
+    for($i = 0;$i < count($categorias);$i++)
+    {
+        if($categorias[$i] == 'Adulto')
+        {   
+            echo "O Competidor ". $nome." Com Idade,".$idade.". Está na categoria: ".$categorias[$i];
+        };
+    };
+}
